fb80f9ad7de685ad4a30b19896a58d23a2e700f7
Daniel Green <KasumiL5x@gmail.com>
2018-04-11 18:34:01 +0100

    Initial commit.

---
+++ Novella/Novella/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Novella
+//
+//  Created by Daniel Green on 11/04/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
