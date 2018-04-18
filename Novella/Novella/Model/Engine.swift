d79f6ecbb584d2262d0b25a117d5ad2a878ddbf1
Daniel Green <KasumiL5x@gmail.com>
2018-04-18 12:57:29 +0100

    Added simple storage and finding of Identifiable objects.

--- Novella/Novella/Model/Engine.swift
+++ Novella/Novella/Model/Engine.swift
@@ -11,7 +11,20 @@ import Foundation
 class Engine {
 	let _story: Story
 	
+	// All objects that are Identifiable. Can be searched.
+	var _identifiables: [Identifiable]
+	
 	init() {
 		self._story = Story()
+		self._identifiables = []
+	}
+	
+	// MARK: Identifiables
+	func find(uuid: String) throws -> Identifiable {
+		let uuidObj = NSUUID(uuidString: uuid)
+		guard let element = _identifiables.first(where: {$0.UUID == uuidObj}) else {
+			throw Errors.invalid("Tried to find by UUID but no match was found (\(uuid)).")
+		}
+		return element
 	}
 }
