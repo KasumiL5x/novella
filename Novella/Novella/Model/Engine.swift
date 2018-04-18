3af78209045d978687e9c97aa97c75f331c5a2ed
Daniel Green <KasumiL5x@gmail.com>
2018-04-18 12:52:19 +0100

    Added Engine. See more for details. This class will contain a Story and become the manager/owner of all objects.  This way, control is maintained over the lifetime of objects.  I am about to decouple the parenting of objects and have them all use their UUID instead.  Classes still maintain references to their children (etc.) but the links will all be able to be set up using using UUIDs searchable from the Engine.

---
+++ Novella/Novella/Model/Engine.swift
@@ -0,0 +1,17 @@
+//
+//  Engine.swift
+//  Novella
+//
+//  Created by Daniel Green on 18/04/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Foundation
+
+class Engine {
+	let _story: Story
+	
+	init() {
+		self._story = Story()
+	}
+}
