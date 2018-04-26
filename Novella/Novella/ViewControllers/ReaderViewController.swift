3a47f07b26b4ec16904ae494d322716faf43b01c
Daniel Green <KasumiL5x@gmail.com>
2018-04-26 23:28:32 +0100

    Started parsing on reader.

--- Novella/Novella/ViewControllers/ReaderViewController.swift
+++ Novella/Novella/ViewControllers/ReaderViewController.swift
@@ -9,7 +9,41 @@
 import Cocoa
 
 class ReaderViewController: NSViewController {
+	var _story: Story?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+	}
+	
+	@IBAction func onOpen(_ sender: NSButton) {
+		// open file
+		let ofd = NSOpenPanel()
+		ofd.title = "Select Novella JSON."
+		ofd.canChooseDirectories = false
+		ofd.canChooseFiles = true
+		ofd.allowsMultipleSelection = false
+		ofd.allowedFileTypes = ["json"]
+		if ofd.runModal() != NSApplication.ModalResponse.OK {
+			print("User cancelled open.")
+			return
+		}
+		
+		// read file contents
+		let contents: String
+		do {
+			contents = try String(contentsOf: ofd.url!)
+		}
+		catch {
+			print("Failed to read file.")
+			return
+		}
+		
+		// parse contents into a Story
+		do {
+			_story = try Story.fromJSON(str: contents)
+		} catch {
+			print("Failed to parse JSON.")
+			return
+		}
 	}
 }
