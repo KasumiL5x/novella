3f9e37e7316801e6b8db7851bb25689488ef025b
Daniel Green <KasumiL5x@gmail.com>
2018-04-26 22:37:39 +0100

    Added empty viewcontroller for reader and writer

---
+++ Novella/Novella/ViewControllers/ReaderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ReaderViewController.swift
+//  Novella
+//
+//  Created by Daniel Green on 26/04/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Cocoa
+
+class ReaderViewController: NSViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
---
+++ Novella/Novella/ViewControllers/WriterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriterViewController.swift
+//  Novella
+//
+//  Created by Daniel Green on 26/04/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Cocoa
+
+class WriterViewController: NSViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
