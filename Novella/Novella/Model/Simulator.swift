8d06f91f40b81a6ad305084b0efd1986e17cbce7
Daniel Green <KasumiL5x@gmail.com>
2018-04-29 18:56:42 +0100

    Added a dummy Simulator class which may eventually be used as a simulation manager for playing the story through in the reader.

---
+++ Novella/Novella/Model/Simulator.swift
@@ -0,0 +1,17 @@
+//
+//  Simulator.swift
+//  Novella
+//
+//  Created by Daniel Green on 29/04/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Foundation
+
+class Simulator {
+	var _story: Story?
+	
+	init(story: Story) {
+		self._story = story
+	}
+}
