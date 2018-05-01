3d8cef2583a0637fcd60b9f3daddeed32ed357ea
Daniel Green <KasumiL5x@gmail.com>
2018-05-01 15:08:42 +0100

    Started on writer and added some test touchbar stuff just to see how it works.

---
+++ Novella/Novella/TouchBarIdentifiers.swift
@@ -0,0 +1,15 @@
+//
+//  TouchBarIdentifiers.swift
+//  Novella
+//
+//  Created by Daniel Green on 01/05/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import AppKit
+
+class TouchBarIdentifiers {
+	static let Test = NSTouchBar.CustomizationIdentifier(rawValue: "me.dgreen.novella.test")
+	
+	static let InfoLabelItem = NSTouchBarItem.Identifier(rawValue: "me.dgreen.novella.infoLabelItem")
+}
--- Novella/Novella/ViewControllers/WriterViewController.swift
+++ Novella/Novella/ViewControllers/WriterViewController.swift
@@ -12,4 +12,40 @@ class WriterViewController: NSViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
+	
+	override func viewDidAppear() {
+		super.viewDidAppear()
+		
+		// https://stackoverflow.com/questions/42342231/how-to-show-touch-bar-in-a-viewcontroller
+		self.view.window?.unbind(NSBindingName(rawValue: #keyPath(touchBar)))
+		self.view.window?.bind(NSBindingName(rawValue: #keyPath(touchBar)), to: self, withKeyPath: #keyPath(touchBar), options: nil)
+	}
+	deinit {
+		self.view.window?.unbind(NSBindingName(rawValue: #keyPath(touchBar)))
+	}
+}
+
+// MARK: TouchBar
+extension WriterViewController {
+	
+	override func makeTouchBar() -> NSTouchBar? {
+		let touchBar = NSTouchBar()
+		touchBar.delegate = self
+		touchBar.customizationIdentifier = TouchBarIdentifiers.Test
+		touchBar.defaultItemIdentifiers = [TouchBarIdentifiers.InfoLabelItem]
+		touchBar.customizationAllowedItemIdentifiers = [TouchBarIdentifiers.InfoLabelItem]
+		touchBar.principalItemIdentifier = TouchBarIdentifiers.InfoLabelItem
+		return touchBar
+	}
+}
+
+extension WriterViewController: NSTouchBarDelegate {
+	func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
+		switch identifier {
+		case TouchBarIdentifiers.InfoLabelItem:
+			return NSColorPickerTouchBarItem.colorPicker(withIdentifier: identifier)
+		default:
+			return nil
+		}
+	}
 }
