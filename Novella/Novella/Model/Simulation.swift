a8df518a6b1d221baffcc703059a999770573596
Daniel Green <KasumiL5x@gmail.com>
2018-05-02 19:42:49 +0100

    Added empty Simulation class that will eventually track progress and manage a single runthrough of the story.

---
+++ Novella/Novella/Model/Simulation.swift
@@ -0,0 +1,13 @@
+//
+//  Simulation.swift
+//  Novella
+//
+//  Created by Daniel Green on 02/05/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Foundation
+
+class Simulation {
+	
+}
