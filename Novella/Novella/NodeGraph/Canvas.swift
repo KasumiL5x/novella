24cbc2679af5e6c27163a9ba89cb6b2c2242bd6c
Daniel Green <KasumiL5x@gmail.com>
2018-05-06 09:43:33 +0100

    Canvas' grid is now a separate view

--- Novella/Novella/NodeGraph/Canvas.swift
+++ Novella/Novella/NodeGraph/Canvas.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class Canvas: NSView {
+	// background grid
+	var _grid: GridView
+	
 	// where nodes are stored
 	var _nodesView: NSView
 	var _canvasWidgets: [CanvasWidget]
@@ -21,6 +24,8 @@ class Canvas: NSView {
 	let _undoRedo: UndoRedo
 	
 	override init(frame frameRect: NSRect) {
+		_grid = GridView(frame: frameRect)
+		
 		_nodesView = NSView(frame: frameRect)
 		_canvasWidgets = []
 		
@@ -33,6 +38,8 @@ class Canvas: NSView {
 		
 		// layers for subviews
 		wantsLayer = true
+		// add background grid
+		self.addSubview(_grid)
 		// add nodes view
 		self.addSubview(_nodesView)
 		// add curves view
@@ -105,72 +112,6 @@ class Canvas: NSView {
 		// updates every curve - not very efficient
 		for child in _curvesView.subviews {
 			child.layer?.setNeedsDisplay()
-		}
-	}
-	
-	override func draw(_ dirtyRect: NSRect) {
-		super.draw(dirtyRect)
-		
-		if let context = NSGraphicsContext.current?.cgContext {
-			context.saveGState()
-			drawGrid(context: context, dirtyRect: dirtyRect)
-			context.restoreGState()
-		}
-	}
-	
-	func drawGrid(context: CGContext, dirtyRect: NSRect) {
-		// solid fill
-		let bgColor = NSColor.init(srgbRed: 54/255.0, green: 54/255.0, blue: 61/255.0, alpha: 1.0)
-		bgColor.setFill()
-		context.fill(dirtyRect)
-		
-		// line variables
-		let lineColor = NSColor.init(srgbRed: 120/255.0, green: 120/255.0, blue: 120/255.0, alpha: 1.0)
-		let majorOpacity = CGFloat(0.5)
-		let minorOpacity = CGFloat(0.4)
-		let defaultOpacity = CGFloat(0.2)
-		let majorThickness = CGFloat(2.0)
-		let minorThickness = CGFloat(1.5)
-		let defaultThickness = CGFloat(1.0)
-		let linePath = NSBezierPath()
-		let majorDivisor = 10
-		let minorDivisor = 5
-		let depth = CGFloat(10.0)
-		
-		// horizontal lines
-		for i in 1..<Int(bounds.size.height / depth) {
-			if i % majorDivisor == 0 {
-				lineColor.withAlphaComponent(majorOpacity).set()
-				linePath.lineWidth = majorThickness
-			} else if i % minorDivisor == 0 {
-				lineColor.withAlphaComponent(minorOpacity).set()
-				linePath.lineWidth = minorThickness
-			} else {
-				lineColor.withAlphaComponent(defaultOpacity).set()
-				linePath.lineWidth = defaultThickness
-			}
-			linePath.removeAllPoints()
-			linePath.move(to: NSPoint(x: 0, y: CGFloat(i) * depth - 0.5))
-			linePath.line(to: NSPoint(x: bounds.size.width, y: CGFloat(i) * depth - 0.5))
-			linePath.stroke()
-		}
-		
-		// vertical lines
-		for i in 1..<Int(bounds.size.height / depth) {
-			if i % majorDivisor == 0 {
-				lineColor.withAlphaComponent(majorOpacity).set()
-				linePath.lineWidth = majorThickness
-			} else if i % minorDivisor == 0 {
-				lineColor.withAlphaComponent(minorOpacity).set()
-				linePath.lineWidth = minorThickness
-			} else {
-				lineColor.withAlphaComponent(defaultOpacity).set()
-				linePath.lineWidth = defaultThickness
-			}
-			linePath.removeAllPoints()
-			linePath.move(to: NSPoint(x: CGFloat(i) * depth - 0.5, y: 0))
-			linePath.line(to: NSPoint(x: CGFloat(i) * depth - 0.5, y: bounds.size.height))
-			linePath.stroke()
 		}
 	}
 }
---
+++ Novella/Novella/NodeGraph/GridView.swift
@@ -0,0 +1,104 @@
+//
+//  BackgroundGrid.swift
+//  Novella
+//
+//  Created by Daniel Green on 06/05/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import AppKit
+
+class GridView: NSView {
+	// color
+	var BackgroundColor: NSColor
+	var LineColor: NSColor
+	// opacity
+	var MajorOpacity: CGFloat
+	var MinorOpacity: CGFloat
+	var Opacity: CGFloat
+	// thickness
+	var MajorThickness: CGFloat
+	var MinorThickness: CGFloat
+	var Thickness: CGFloat
+	// divisors
+	var MajorDivisor: Int
+	var MinorDivisor: Int
+	// depth
+	var Depth: CGFloat
+	
+	
+	override init(frame frameRect: NSRect) {
+		self.BackgroundColor = NSColor.fromHex("#36363d")
+		self.LineColor = NSColor.fromHex("#787878")
+		//
+		self.MajorOpacity = CGFloat(0.5)
+		self.MinorOpacity = CGFloat(0.4)
+		self.Opacity = CGFloat(0.2)
+		//
+		self.MajorThickness = CGFloat(2.0)
+		self.MinorThickness = CGFloat(1.5)
+		self.Thickness = CGFloat(1.0)
+		//
+		self.MajorDivisor = 10
+		self.MinorDivisor = 5
+		//
+		self.Depth = CGFloat(10.0)
+		
+		super.init(frame: frameRect)
+	}
+	required init?(coder decoder: NSCoder) {
+		fatalError("GridView::init(coder) not implemented.")
+	}
+	
+	override func draw(_ dirtyRect: NSRect) {
+		super.draw(dirtyRect)
+		
+		if let context = NSGraphicsContext.current?.cgContext {
+			context.saveGState()
+			
+			// solid fill
+			BackgroundColor.setFill()
+			context.fill(dirtyRect)
+			
+			let linePath = NSBezierPath()
+			
+			// horizontal lines
+			for i in 1..<Int(bounds.size.height / Depth) {
+				if i % MajorDivisor == 0 {
+					LineColor.withAlphaComponent(MajorOpacity).set()
+					linePath.lineWidth = MajorThickness
+				} else if i % MinorDivisor == 0 {
+					LineColor.withAlphaComponent(MinorOpacity).set()
+					linePath.lineWidth = MinorThickness
+				} else {
+					LineColor.withAlphaComponent(Opacity).set()
+					linePath.lineWidth = Thickness
+				}
+				linePath.removeAllPoints()
+				linePath.move(to: NSPoint(x: 0, y: CGFloat(i) * Depth - 0.5))
+				linePath.line(to: NSPoint(x: bounds.size.width, y: CGFloat(i) * Depth - 0.5))
+				linePath.stroke()
+			}
+			
+			// vertical lines
+			for i in 1..<Int(bounds.size.height / Depth) {
+				if i % MajorDivisor == 0 {
+					LineColor.withAlphaComponent(MajorOpacity).set()
+					linePath.lineWidth = MajorThickness
+				} else if i % MinorDivisor == 0 {
+					LineColor.withAlphaComponent(MinorOpacity).set()
+					linePath.lineWidth = MinorThickness
+				} else {
+					LineColor.withAlphaComponent(Opacity).set()
+					linePath.lineWidth = Thickness
+				}
+				linePath.removeAllPoints()
+				linePath.move(to: NSPoint(x: CGFloat(i) * Depth - 0.5, y: 0))
+				linePath.line(to: NSPoint(x: CGFloat(i) * Depth - 0.5, y: bounds.size.height))
+				linePath.stroke()
+			}
+			
+			context.restoreGState()
+		}
+	}
+}
