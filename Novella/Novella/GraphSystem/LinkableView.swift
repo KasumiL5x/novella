993cd361244bfb24bef9d584c4ca5c4300dd3ced
Daniel Green <KasumiL5x@gmail.com>
2018-05-12 17:02:21 +0100

    Going to now rely upon gestures for movement in linkable widgets. Please read full commit.
    Right now linkablewidget has a faked widgetRect() which will change next commit. I tested the gestures and they use hitTest() which means they abide to my widgetRect()!

--- Novella/Novella/GraphSystem/LinkableView.swift
+++ Novella/Novella/GraphSystem/LinkableView.swift
@@ -10,21 +10,100 @@ import Cocoa
 import NovellaModel
 
 class LinkableView: NSView {
-	fileprivate var _nvLinkable: NVLinkable?
+	// MARK: - - Variables -
+	fileprivate var _nvLinkable: NVLinkable
+	fileprivate var _isPrimed: Bool
+	fileprivate var _isSelected: Bool
+	//
+	var _clickGesture: NSClickGestureRecognizer?
+	var _ctxGesture: NSClickGestureRecognizer?
+	var _panGesture: NSPanGestureRecognizer?
 	
+	// MARK: - - Initialization -
 	init(frameRect: NSRect, nvLinkable: NVLinkable) {
 		self._nvLinkable = nvLinkable
+		self._isPrimed = false
+		self._isSelected = false
+		self._clickGesture = nil
+		self._ctxGesture = nil
+		self._panGesture = nil
 		super.init(frame: frameRect)
+		
+		// primary click recognizer
+		_clickGesture = NSClickGestureRecognizer(target: self, action: #selector(LinkableView.onClick))
+		_clickGesture!.buttonMask = 0x1 // "primary click"
+		_clickGesture!.numberOfClicksRequired = 1
+		self.addGestureRecognizer(_clickGesture!)
+		// context click recognizer
+		_ctxGesture = NSClickGestureRecognizer(target: self, action: #selector(LinkableView.onContextClick))
+		_ctxGesture!.buttonMask = 0x2 // "secondary click"
+		_ctxGesture!.numberOfClicksRequired = 1
+		self.addGestureRecognizer(_ctxGesture!)
+		// pan regoznizer
+		_panGesture = NSPanGestureRecognizer(target: self, action: #selector(LinkableView.onPan))
+		_panGesture!.buttonMask = 0x1 // "primary click"
+		self.addGestureRecognizer(_panGesture!)
 	}
 	required init?(coder decoder: NSCoder) {
 		fatalError("LinkableView::init(coder) not implemented.")
 	}
 	
+	// MARK: - - Properties -
+	var Linkable: NVLinkable {
+		get{ return _nvLinkable }
+	}
+	var IsPrimed: Bool {
+		get{ return _isPrimed }
+	}
+	var IsSelected: Bool {
+		get{ return _isSelected }
+	}
+	
+	// MARK: - - Functions -
+	// MARK: Hit Test
+	override func hitTest(_ point: NSPoint) -> NSView? {
+		// point is in the superview's coordinate system
+		// manually check each subview's bounds (if i want to do something similar i'd need to override their hittest and call it here)
+		for sub in subviews {
+			if NSPointInRect(superview!.convert(point, to: sub), sub.bounds) {
+				return sub
+			}
+		}
+		
+		// check out local widget's bounds for a mouse click
+		if NSPointInRect(superview!.convert(point, to: self), widgetRect()) {
+			return self
+		}
+		
+		// nuttin'
+		return nil
+	}
+	
+	// MARK: Gesture Callbacks
+	@objc fileprivate func onClick(gesture: NSGestureRecognizer) {
+		print("clicked \(gesture.location(in: self))")
+	}
+	@objc fileprivate func onContextClick(gesture: NSGestureRecognizer) {
+		print("context clicked \(gesture.location(in: self))")
+	}
+	@objc fileprivate func onPan(gesture: NSGestureRecognizer) {
+		print("panned \(_panGesture!.translation(in: self))")
+	}
+	// MARK: Virtual Functions
+	func widgetRect() -> NSRect {
+//		print("LinkableView::widgetRect() should be overridden.")
+		return NSMakeRect(0, 0, 10, 10)
+//		return NSRect.zero
+	}
+	
 	override func draw(_ dirtyRect: NSRect) {
 		if let context = NSGraphicsContext.current?.cgContext {
 			context.saveGState()
-			let path = NSBezierPath(roundedRect: bounds, xRadius: 2.0, yRadius: 2.0)
+			var path = NSBezierPath(roundedRect: bounds, xRadius: 2.0, yRadius: 2.0)
 			NSColor.red.setFill()
+			path.fill()
+			path = NSBezierPath(roundedRect: widgetRect(), xRadius: 2.0, yRadius: 2.0)
+			NSColor.green.setFill()
 			path.fill()
 			context.restoreGState()
 		}
