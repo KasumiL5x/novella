33dab942b8efe117daebdf414f808d244739ced1
Daniel Green <KasumiL5x@gmail.com>
2018-05-20 10:55:30 +0100

    Implemented callbacks ready to set dialog variables.

--- Novella/Novella/ViewControllers/Popovers/GraphDialogPopoverViewController.swift
+++ Novella/Novella/ViewControllers/Popovers/GraphDialogPopoverViewController.swift
@@ -36,15 +36,65 @@ class GraphDialogPopoverViewController: NSViewController {
 		let dlg = (_dialogNode!.Linkable as! NVDialog)
 		
 		let name = dlg.Name
-		_nameTextField.stringValue = name.isEmpty ? "No name" : name
+		_nameTextField.stringValue = name.isEmpty ? "" : name
 		
 		let dirs = dlg.Preview
-		_directionsTextField.stringValue = dirs.isEmpty ? "No directions" : dirs
+		_directionsTextField.stringValue = dirs.isEmpty ? "" : dirs
 		
 		let prev = dlg.Preview
-		_previewTextField.stringValue = prev.isEmpty ? "No preview" : prev
+		_previewTextField.stringValue = prev.isEmpty ? "" : prev
 		
 		let content = dlg.Content
-		_contentTextField.stringValue = content.isEmpty ? "No content" : content
+		_contentTextField.stringValue = content.isEmpty ? "" : content
 	}
+	
+	@IBAction func onNameChanged(_ sender: NSTextField) {
+		if _dialogNode == nil {
+			return
+		}
+		
+		let oldName = (_dialogNode!.Linkable as! NVDialog).Name
+		let newName = sender.stringValue
+		
+		// don't forget to revert sender if name change fails
+		
+		print("Name changed from \(oldName) to \(newName) (but not implemented yet)")
+	}
+	
+	@IBAction func onDirectionsChanged(_ sender: NSTextField) {
+		if _dialogNode == nil {
+			return
+		}
+		
+		let oldDir = (_dialogNode!.Linkable as! NVDialog).Directions
+		let newDir = sender.stringValue
+		
+		print("Directions changed from \(oldDir) to \(newDir) (but not implemented yet)")
+	}
+	
+	@IBAction func onPreviewChanged(_ sender: NSTextField) {
+		if _dialogNode == nil {
+			return
+		}
+		
+		let oldPrev = (_dialogNode!.Linkable as! NVDialog).Preview
+		let newPrev = sender.stringValue
+		
+		print("Preview changed from \(oldPrev) to \(newPrev) (but not implemented yet)")
+	}
+	
+	@IBAction func onContentChanged(_ sender: NSTextField) {
+		if _dialogNode == nil {
+			return
+		}
+		
+		let oldContent = (_dialogNode!.Linkable as! NVDialog).Content
+		let newContent = sender.stringValue
+		
+		print("Content changed from \(oldContent) to \(newContent) (but not implemented yet)")
+	}
+	
+	
+	
+	
 }
