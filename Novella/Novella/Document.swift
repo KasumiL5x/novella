645ee31a8a2e6e4e8d5984d8cae069bef709f991
Daniel Green <KasumiL5x@gmail.com>
2018-06-02 20:29:53 +0100

    Started experimenting with cocoa's document-based app. Read full for more info.
    This is a new project with everything copied over, so it's likely that it has issues.  However, seems to work right now.  Note that l MUST remove the singleton for NVStoryManager otherwise only one instance works.  That, or I use a multiton, but that's messy internally.

---
+++ Novella/Novella/Document.swift
@@ -0,0 +1,57 @@
+//
+//  Document.swift
+//  Novella
+//
+//  Created by Daniel Green on 02/06/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Cocoa
+import NovellaModel
+
+class Document: NSDocument {
+	override init() {
+	    super.init()
+		// Add your subclass-specific initialization here.
+	}
+
+	override class var autosavesInPlace: Bool {
+		return true
+	}
+
+	override func makeWindowControllers() {
+		// Returns the Storyboard that contains your Document window.
+		let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+		let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+		self.addWindowController(windowController)
+		Swift.print("makeWC")
+	}
+
+	override func data(ofType typeName: String) throws -> Data {
+		
+		let jsonStr = NVStoryManager.shared.toJSON()
+		if let data = jsonStr.data(using: .utf8) {
+			return data
+		}
+		
+		// Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
+		// You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	}
+
+	override func read(from data: Data, ofType typeName: String) throws {
+		// Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
+		// You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
+		// If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
+		
+		if let jsonStr = String(data: data, encoding: .utf8) {
+			NVStoryManager.fromJSON(str: jsonStr)
+			return
+		}
+		
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	}
+
+
+}
+
