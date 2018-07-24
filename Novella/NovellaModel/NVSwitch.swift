b7acb0e70bdd8a2581adf80e6ef2b3fce4e065a1
Daniel Green <KasumiL5x@gmail.com>
2018-07-24 20:00:46 +0100

    Added  a function to NVSwitch to let it evaluate and provide a resulting transfer.

--- Novella/NovellaModel/NVSwitch.swift
+++ Novella/NovellaModel/NVSwitch.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 public class NVSwitch : NVBaseLink {
+	static let EPSILON: Double = 0.001 // change to alter precision of double matching
+	
 	// MARK: - Variables -
 	private var _variable: NVVariable?
 	private var _defaultTransfer: NVTransfer
@@ -20,5 +22,47 @@ public class NVSwitch : NVBaseLink {
 		self._defaultTransfer = NVTransfer(manager: manager)
 		self._values = [:]
 		super.init(manager: manager, uuid: uuid, origin: origin)
+	}
+	
+	// MARK: - Functions -
+	public func evaluate() -> NVTransfer {
+		// no variable - default transfer
+		guard let variable = _variable else {
+			return _defaultTransfer
+		}
+		
+		// switch values and return matching transfer
+		for val in _values {
+			// handle each data type separately as we can't directly compare Any/AnyHashable
+			switch variable.DataType {
+			case .boolean:
+				// same data type
+				if !(val.key is Bool) {
+					break
+				}
+				if (val.key as! Bool) == (variable.Value as! Bool) {
+					return val.value
+				}
+				
+			case .integer:
+				if !(val.key is Int) {
+					break
+				}
+				if (val.key as! Int) == (variable.Value as! Int) {
+					return val.value
+				}
+				
+			case .double:
+				if !(val.key is Double) {
+					break
+				}
+				if fabs((val.key as! Double) - (variable.Value as! Double)) <= NVSwitch.EPSILON {
+					return val.value
+				}
+			}
+		}
+		
+		// nothing matched - return default
+		return _defaultTransfer
 	}
 }
