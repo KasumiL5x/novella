87bf87debc49a52ed5770cc403a9f45f5fd326db
Daniel Green <KasumiL5x@gmail.com>
2018-12-11 21:10:04 +0000

    Started to add dragging to the CanvasLink.

--- Novella/Novella/canvas/CanvasLink.swift
+++ Novella/Novella/canvas/CanvasLink.swift
@@ -15,10 +15,18 @@ class CanvasLink: NSView {
 	
 	private let _outlineLayer: CAShapeLayer
 	private let _fillLayer: CAShapeLayer
+	//
+	private let _dragLayer: CAShapeLayer
+	private var _isDragging: Bool
+	private var _dragPosition: CGPoint
 	
 	init() {
 		self._outlineLayer = CAShapeLayer()
 		self._fillLayer = CAShapeLayer()
+		//
+		self._dragLayer = CAShapeLayer()
+		self._isDragging = false
+		self._dragPosition = CGPoint.zero
 		super.init(frame: NSMakeRect(0, 0, CanvasLink.Size, CanvasLink.Size))
 		
 		wantsLayer = true
@@ -44,9 +52,58 @@ class CanvasLink: NSView {
 		_fillLayer.fillColor = CGColor.clear
 		_fillLayer.path = NSBezierPath(ovalIn: fillRect).cgPath
 		layer?.addSublayer(_fillLayer)
+		
+		// pan gesture
+		let panGesture = NSPanGestureRecognizer(target: self, action: #selector(CanvasLink.onPan))
+		panGesture.buttonMask = 0x1
+		addGestureRecognizer(panGesture)
+		
+		// drag layer
+		_dragLayer.fillColor = nil
+		_dragLayer.lineCap = .round
+		_dragLayer.lineDashPattern = [5, 5]
+		_dragLayer.lineWidth = 2.0
+		_dragLayer.strokeColor = CGColor.clear
+		layer?.addSublayer(_dragLayer)
 	}
-	
 	required init?(coder decoder: NSCoder) {
 		fatalError()
+	}
+	
+	@objc private func onPan(gesture: NSPanGestureRecognizer) {
+		switch gesture.state {
+		case .began:
+			_isDragging = true
+			_dragPosition = gesture.location(in: self)
+			_dragLayer.strokeColor = NSColor.red.cgColor
+			_dragLayer.removeAllAnimations()
+			
+		case .changed:
+			_dragPosition = gesture.location(in: self)
+			let origin = NSMakePoint(bounds.midX, bounds.midY)
+			_dragLayer.path = CurveHelper.catmullRom(points: [origin, _dragPosition], alpha: 1.0, closed: false).cgPath
+			
+		case .cancelled, .ended:
+			_isDragging = false
+			// animate color to clear
+			let colorAnim = CABasicAnimation(keyPath: "strokeColor")
+			colorAnim.duration = 0.3
+			colorAnim.toValue = CGColor.clear
+			colorAnim.fillMode = .forwards
+			colorAnim.isRemovedOnCompletion = false
+			_dragLayer.add(colorAnim, forKey: colorAnim.keyPath)
+			// animate stroke length to 0
+			let strokeAnim = CABasicAnimation(keyPath: "strokeEnd")
+			strokeAnim.toValue = 0.0
+			strokeAnim.duration = 0.2
+			strokeAnim.fillMode = .forwards
+			strokeAnim.isRemovedOnCompletion = false
+			_dragLayer.add(strokeAnim, forKey: strokeAnim.keyPath)
+			
+		default:
+			break
+		}
+		
+		setNeedsDisplay(bounds)
 	}
 }
---
+++ Novella/Novella/utils/CurveHelper.swift
@@ -0,0 +1,113 @@
+//
+//  CurveHelper.swift
+//  novella
+//
+//  Created by Daniel Green on 11/12/2018.
+//  Copyright © 2018 dgreen. All rights reserved.
+//
+
+import AppKit
+
+class CurveHelper {
+	static func catmullRom(points: [NSPoint], alpha: CGFloat, closed: Bool) -> NSBezierPath {
+		let path = NSBezierPath()
+		var points = points // local copy
+		
+		// supplement 2 points if only start and end provided
+		if points.count == 2 {
+			let spacing: CGFloat = 20.0
+			let start = points[0], end = points[1]
+			let diff = abs(end.x - start.x)
+			let gap = diff <= spacing ? diff : spacing // anything below spacing, just use the diff
+			
+			points.insert(start + NSMakePoint(gap, 0.0), at: 1)
+			points.insert(end - NSMakePoint(gap, 0.0), at: 2)
+		}
+		
+		if points.count < 4 {
+			return path
+		}
+		
+		let endIndex = closed ? points.count : points.count-2
+		let startIndex = closed ? 0 : 1
+		
+		for idx in startIndex..<endIndex {
+			let nextIdx = (idx+1) % points.count
+			let nextNextIdx = (idx+1) % points.count + 1//(nextIdx+1) % points.count
+			let prevIdx = idx-1 < 0 ? points.count-1 : idx-1
+			
+			let p1 = points[idx]
+			let p0 = points[prevIdx]
+			let p2 = points[nextIdx]
+			let p3 = points[nextNextIdx]
+			
+			let d1 = (p1 - p0).length()
+			let d2 = (p2 - p1).length()
+			let d3 = (p3 - p2).length()
+			
+			var b1: NSPoint
+			if abs(d1) < CGFloat.ulpOfOne {
+				b1 = p1
+			} else {
+				b1 = p2 * pow(d1, 2.0*alpha)
+				b1 = b1 - (p0 * pow(d2, 2.0*alpha))
+				b1 = b1 + (p1 * (2.0*pow(d1, 2.0*alpha) + 3.0*pow(d1, alpha)*pow(d2, alpha) + pow(d2, 2.0*alpha)))
+				b1 = b1 * (1.0 / (3.0*pow(d1, alpha)*(pow(d1, alpha)+pow(d2, alpha))))
+			}
+			
+			var b2: NSPoint
+			if abs(d3) < CGFloat.ulpOfOne {
+				b2 = p2
+			} else {
+				b2 = p1 * pow(d3, 2.0*alpha)
+				b2 = b2 - (p3 * pow(d2, 2.0*alpha))
+				b2 = b2 + (p2 * (2.0*pow(d3, 2.0*alpha) + 3.0*pow(d3, alpha)*pow(d2, alpha) + pow(d2, 2.0*alpha)))
+				b2 = b2 * (1.0 / (3.0*pow(d3, alpha)*(pow(d3, alpha)+pow(d2, alpha))))
+			}
+			
+			if idx == startIndex {
+				path.move(to: p0)// only works if p0 instead of p1
+			}
+			
+			// only works if p3 instead of p2
+			path.curve(to: p3, controlPoint1: b1, controlPoint2: b2)
+		}
+		
+		if closed {
+			path.close()
+		}
+		
+		return path
+	}
+	
+	static func line(start: NSPoint, end: NSPoint) -> NSBezierPath {
+		let path = NSBezierPath()
+		path.move(to: start)
+		path.line(to: end)
+		return path
+	}
+	
+	static func square(start: NSPoint, end: NSPoint) -> NSBezierPath {
+		let path = NSBezierPath()
+		let spacing: CGFloat = 20.0
+		let diff = end.x - start.x
+		
+		path.move(to: start)
+		
+		// on the right side
+		if diff > 0.0 {
+			let gap = diff <= spacing ? diff : spacing // anything below spacing, just use the diff
+			path.line(to: start + NSMakePoint(gap, 0.0))
+			path.line(to: NSMakePoint(start.x + gap, end.y))
+			path.line(to: end)
+		} else {
+			let midY = (start.y + end.y) * 0.5
+			path.line(to: start + NSMakePoint(spacing, 0.0))
+			path.line(to: NSMakePoint(start.x + spacing, midY))
+			path.line(to: NSMakePoint(end.x - spacing, midY))
+			path.line(to: NSMakePoint(end.x - spacing, end.y))
+			path.line(to: end)
+		}
+		return path
+	}
+}
