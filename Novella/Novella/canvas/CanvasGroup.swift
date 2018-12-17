b751de8cda47ed9e71ab52d6dce3e664ce8bcd7b
Daniel Green <KasumiL5x@gmail.com>
2018-12-17 15:13:58 +0000

    First steps at redesigning all of the nodes. Testing in CanvasGroup. Need to add an icon in the flag area, implement parallel and entry circles, and add a label. It's flexible right now and resizing for larger nodes works nicely. Added a large func to NSBezierPath too.

--- Novella/Novella/canvas/CanvasGroup.swift
+++ Novella/Novella/canvas/CanvasGroup.swift
@@ -9,34 +9,51 @@
 import Cocoa
 
 class CanvasGroup: CanvasObject {
+	static let Roundness: CGFloat = 0.075
+	static let NormalOutlineSize: CGFloat = 1.0
+	static let PrimedOutlineSize: CGFloat = 2.0
+	static let SelectedOutlineSize: CGFloat = 4.0
+	static let FlagSize: CGFloat = 0.3
+	
 	let Group: NVGroup
+	
+	private let _outlineLayer: CAShapeLayer
 	
 	init(canvas: Canvas, group: NVGroup) {
 		self.Group = group
-		super.init(canvas: canvas, frame: NSMakeRect(0, 0, 64, 64))
+		self._outlineLayer = CAShapeLayer()
+		super.init(canvas: canvas, frame: NSMakeRect(0, 0, 1, 1))
+		self.frame = objectRect()
 		
 		wantsLayer = true
 		layer?.masksToBounds = false
 		
-		ContextMenu.addItem(withTitle: "Submerge", action: #selector(CanvasGroup.onSubmerge), keyEquivalent: "")
+		let mainFrame = objectRect()
 		
-		let bgOutline = CAGradientLayer(layer: layer)
-		bgOutline.colors = [NSColor.fromHex("#FFFFFF").cgColor, NSColor.fromHex("#D7E1EC").cgColor]
-		bgOutline.locations = [0.0, 1.0]
-		bgOutline.startPoint = CGPoint.zero
-		bgOutline.endPoint = NSMakePoint(0.0, 1.0)
-		bgOutline.frame = bounds.insetBy(dx: -2.0, dy: -2.0)
-		bgOutline.cornerRadius = max(bgOutline.frame.width, bgOutline.frame.height) * 0.5
-		layer?.addSublayer(bgOutline)
-		//
-		let bgGradient = CAGradientLayer(layer: layer)
-		bgGradient.colors = [NSColor.fromHex("#FFFFFF").cgColor, NSColor.fromHex("#D7E1EC").cgColor]
-		bgGradient.locations = [0.0, 1.0]
-		bgGradient.startPoint = NSMakePoint(0.0, 1.0)
-		bgGradient.endPoint = CGPoint.zero
-		bgGradient.frame = bounds
-		bgGradient.cornerRadius = max(bgGradient.frame.width, bgGradient.frame.height) * 0.5
+		// main background layer
+		let bgGradient = CAGradientLayer()
+		bgGradient.frame = mainFrame
+		bgGradient.cornerRadius = (max(bgGradient.frame.width, bgGradient.frame.height) * 0.5) * CanvasGroup.Roundness
+		bgGradient.colors = [NSColor.fromHex("#A6BBCF").cgColor, NSColor.fromHex("#b3c5d6").cgColor]
+		bgGradient.startPoint = NSPoint.zero
+		bgGradient.endPoint = NSMakePoint(0.0, 1.0)
+		bgGradient.locations = [0.0, 0.3]
+		// outline layer
+		_outlineLayer.fillColor = nil
+		_outlineLayer.path = NSBezierPath(roundedRect: bgGradient.frame, xRadius: bgGradient.cornerRadius, yRadius: bgGradient.cornerRadius).cgPath
+		_outlineLayer.strokeColor = NSColor.fromHex("#b3c5d6").withAlphaComponent(0.4).cgColor //99b1c8
+		setupOutlineLayer()
+		layer?.addSublayer(_outlineLayer) // add before bg so it's below it
 		layer?.addSublayer(bgGradient)
+		
+		// flag layer
+		let flagLayer = CAShapeLayer()
+		let flagRect = NSMakeRect(0, 0, mainFrame.width * CanvasGroup.FlagSize, mainFrame.height)
+		flagLayer.path = NSBezierPath.withRoundedCorners(rect: flagRect, byRoundingCorners: [.minXMinY, .minXMaxY], withRadius: bgGradient.cornerRadius, includingEdges: [.all]).cgPath
+		flagLayer.fillColor = NSColor.fromHex("#99b1c8").cgColor
+		layer?.addSublayer(flagLayer)
+		
+		ContextMenu.addItem(withTitle: "Submerge", action: #selector(CanvasGroup.onSubmerge), keyEquivalent: "")
 	}
 	required init?(coder decoder: NSCoder) {
 		fatalError()
@@ -46,6 +63,19 @@ class CanvasGroup: CanvasObject {
 		_canvas.setupFor(group: self.Group)
 	}
 	
+	private func setupOutlineLayer() {
+		let width: CGFloat
+		switch CurrentState {
+		case .normal:
+			width = CanvasGroup.NormalOutlineSize
+		case .primed:
+			width = CanvasGroup.PrimedOutlineSize
+		case .selected:
+			width = CanvasGroup.SelectedOutlineSize
+		}
+		_outlineLayer.lineWidth = width
+	}
+	
 	// virtuals
 	override func mainColor() -> NSColor {
 		return NSColor.fromHex("#FF00FF") // not yet implemented this class fully
@@ -53,12 +83,10 @@ class CanvasGroup: CanvasObject {
 	override func onMove() {
 		_canvas.Doc.Positions[Group.UUID] = frame.origin
 	}
-	
-	override func draw(_ dirtyRect: NSRect) {
-		if let context = NSGraphicsContext.current?.cgContext {
-			context.saveGState()
-			
-			context.restoreGState()
-		}
+	override func onStateChanged() {
+		setupOutlineLayer()
+	}
+	override func objectRect() -> NSRect {
+		return NSMakeRect(0, 0, 100.0, 100.0 * 0.25)
 	}
 }
--- Novella/Novella/utils/NSBezierPath+CGPath.swift
+++ Novella/Novella/utils/NSBezierPath+CGPath.swift
@@ -8,6 +8,33 @@
 
 import AppKit
 
+public struct RectCorner: OptionSet {
+	public let rawValue: Int
+	
+	public init(rawValue: Int) {
+		self.rawValue = rawValue
+	}
+	
+	static let minXMinY = RectCorner(rawValue: 1 << 0)
+	static let maxXMinY = RectCorner(rawValue: 1 << 1)
+	static let maxXMaxY = RectCorner(rawValue: 1 << 2)
+	static let minXMaxY = RectCorner(rawValue: 1 << 3)
+	static let all = RectCorner(rawValue: ~0)
+}
+public struct RectEdge: OptionSet {
+	public let rawValue: Int
+	
+	public init(rawValue: Int) {
+		self.rawValue = rawValue
+	}
+	
+	static let minX = RectEdge(rawValue: 1 << 0)
+	static let maxX = RectEdge(rawValue: 1 << 1)
+	static let minY = RectEdge(rawValue: 1 << 2)
+	static let maxY = RectEdge(rawValue: 1 << 3)
+	static let all = RectEdge(rawValue: ~0)
+}
+
 public extension NSBezierPath {
 	public var cgPath: CGPath {
 		let path = CGMutablePath()
@@ -22,5 +49,162 @@ public extension NSBezierPath {
 			}
 		}
 		return path
+	}
+	
+	// converted to swift from this lovely source: https://github.com/omnigroup/OmniGroup/blob/master/Frameworks/OmniAppKit/OpenStepExtensions.subproj/NSBezierPath-OAExtensions.m
+	class func withRoundedCorners(rect: NSRect, byRoundingCorners corners: RectCorner, withRadius radius: CGFloat, includingEdges edges: RectEdge) -> NSBezierPath {
+		// This is the value AppKit uses in -appendBezierPathWithRoundedRect:xRadius:yRadius:
+		let kControlPointMultiplier: CGFloat = 0.55228
+		
+		// empty rect
+		if NSIsEmptyRect(rect) {
+			return NSBezierPath()
+		}
+		
+		let bezierPath = NSBezierPath()
+		var startPoint: NSPoint
+		var sourcePoint: NSPoint
+		var destPoint: NSPoint
+		var controlPoint1: NSPoint
+		var controlPoint2: NSPoint
+		
+		let length = min(NSWidth(rect), NSHeight(rect))
+		let radius = min(radius, length / 2.0)
+		
+		// Top Left (in terms of a non-flipped view)
+		var includeCorner = edges.contains(.minX) || edges.contains(.minY)
+		if corners.contains(.minXMinY) {
+			sourcePoint = NSMakePoint(NSMinX(rect), NSMaxY(rect) - radius)
+			startPoint = sourcePoint // capture for "closing" path without necessarily adding a segment for the final edge
+			
+			destPoint = NSMakePoint(NSMinX(rect) + radius, NSMaxY(rect))
+			
+			if includeCorner {
+				controlPoint1 = sourcePoint
+				controlPoint1.y += radius * kControlPointMultiplier
+				
+				controlPoint2 = destPoint
+				controlPoint2.x -= radius * kControlPointMultiplier
+				
+				bezierPath.move(to: sourcePoint)
+				bezierPath.curve(to: destPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		} else {
+			startPoint = NSMakePoint(NSMinX(rect), NSMaxY(rect))  // capture for "closing" path without necessarily adding a segment for the final edge
+			bezierPath.move(to: startPoint)
+		}
+		
+		// Top right (in terms of a flipped view)
+		var includeEdge = edges.contains(.minY)
+		includeCorner = edges.contains(.minY) || edges.contains(.maxX)
+		if corners.contains(.maxXMinY) {
+			sourcePoint = NSMakePoint(NSMaxX(rect) - radius, NSMaxY(rect))
+			destPoint = NSMakePoint(NSMaxX(rect), NSMaxY(rect) - radius)
+			
+			if includeEdge {
+				bezierPath.line(to: sourcePoint)
+			} else {
+				bezierPath.move(to: sourcePoint)
+			}
+			
+			if includeCorner {
+				controlPoint1 = sourcePoint
+				controlPoint1.x += radius * kControlPointMultiplier
+				
+				controlPoint2 = destPoint
+				controlPoint2.y += radius * kControlPointMultiplier
+				
+				bezierPath.curve(to: destPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		} else {
+			destPoint = NSMakePoint(NSMaxX(rect), NSMaxY(rect))
+			if includeEdge {
+				bezierPath.line(to: destPoint)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		}
+		
+		// Bottom right (in terms of a flipped view)
+		includeEdge = edges.contains(.maxX)
+		includeCorner = edges.contains(.maxX) || edges.contains(.maxY)
+		if corners.contains(.maxXMaxY) {
+			sourcePoint = NSMakePoint(NSMaxX(rect), NSMinY(rect) + radius)
+			destPoint = NSMakePoint(NSMaxX(rect) - radius, NSMinY(rect))
+			
+			if includeEdge {
+				bezierPath.line(to: sourcePoint)
+			} else {
+				bezierPath.move(to: sourcePoint)
+			}
+			
+			if includeCorner {
+				controlPoint1 = sourcePoint
+				controlPoint1.y -= radius * kControlPointMultiplier
+				
+				controlPoint2 = destPoint
+				controlPoint2.x += radius * kControlPointMultiplier
+				
+				bezierPath.curve(to: destPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		} else {
+			destPoint = NSMakePoint(NSMaxX(rect), NSMinY(rect))
+			if includeEdge {
+				bezierPath.line(to: destPoint)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		}
+		
+		// Bottom left (in terms of a flipped view)
+		includeEdge = edges.contains(.maxY)
+		includeCorner = edges.contains(.maxY) || edges.contains(.minX)
+		if corners.contains(.minXMaxY) {
+			sourcePoint = NSMakePoint(NSMinX(rect) + radius, NSMinY(rect))
+			destPoint = NSMakePoint(NSMinX(rect), NSMinY(rect) + radius)
+			
+			if includeEdge {
+				bezierPath.line(to: sourcePoint)
+			} else {
+				bezierPath.move(to: sourcePoint)
+			}
+			
+			if includeCorner {
+				controlPoint1 = sourcePoint
+				controlPoint1.x -= radius * kControlPointMultiplier
+				
+				controlPoint2 = destPoint
+				controlPoint2.y -= radius * kControlPointMultiplier
+				
+				bezierPath.curve(to: destPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		} else {
+			destPoint = NSMakePoint(NSMinX(rect), NSMinY(rect))
+			if includeEdge {
+				bezierPath.line(to: destPoint)
+			} else {
+				bezierPath.move(to: destPoint)
+			}
+		}
+		
+		// Back to top Left (in terms of a non-flipped view)
+		// CONSIDER: If the top-left corner is rounded, the subpath ends at the beginning of the curve rather than at the top-left corner of the bounding rect (assuming non-flipped coordinates). Is that really what we want if using this for composite paths? Should we do an additional move to (MinX, MinY) of the bounding rect?
+		includeEdge = edges.contains(.minX)
+		if includeEdge {
+			bezierPath.line(to: startPoint)
+		} else {
+			bezierPath.move(to: startPoint)
+		}
+		
+		return bezierPath
 	}
 }
