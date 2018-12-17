8e8ce24177f6d7f197df9e7aed953f120d702e79
Daniel Green <KasumiL5x@gmail.com>
2018-12-17 15:41:03 +0000

    More work on the new node design (added image and text). Will formalize this later and shove it for all nodes. Icons will differentiate the main types, and colors the subtypes. I may want to expand this vertically for the events (and DN beats?) to add extra info, which is totally fine.

--- Novella/Novella/canvas/CanvasGroup.swift
+++ Novella/Novella/canvas/CanvasGroup.swift
@@ -14,14 +14,18 @@ class CanvasGroup: CanvasObject {
 	static let PrimedOutlineSize: CGFloat = 2.0
 	static let SelectedOutlineSize: CGFloat = 4.0
 	static let FlagSize: CGFloat = 0.3
+	static let IconSize: CGFloat = 0.55
+	static let LabelOffset: CGFloat = 4.0
 	
 	let Group: NVGroup
 	
 	private let _outlineLayer: CAShapeLayer
+	private let _labelLayer: CATextLayer
 	
 	init(canvas: Canvas, group: NVGroup) {
 		self.Group = group
 		self._outlineLayer = CAShapeLayer()
+		self._labelLayer = CATextLayer()
 		super.init(canvas: canvas, frame: NSMakeRect(0, 0, 1, 1))
 		self.frame = objectRect()
 		
@@ -52,6 +56,28 @@ class CanvasGroup: CanvasObject {
 		flagLayer.path = NSBezierPath.withRoundedCorners(rect: flagRect, byRoundingCorners: [.minXMinY, .minXMaxY], withRadius: bgGradient.cornerRadius, includingEdges: [.all]).cgPath
 		flagLayer.fillColor = NSColor.fromHex("#99b1c8").cgColor
 		layer?.addSublayer(flagLayer)
+		
+		// type icon layer
+		let iconLayer = CALayer()
+		let typeIconSize = flagRect.width * CanvasGroup.IconSize // square as a percentage of the flag
+		iconLayer.frame = NSMakeRect(0, 0, typeIconSize, typeIconSize)
+		iconLayer.frame.setCenter(flagRect.center)
+		iconLayer.contents = NSImage(named: NSImage.cautionName)
+		iconLayer.contentsRect = NSMakeRect(0, 0, 1, 1)
+		iconLayer.contentsGravity = .resizeAspectFill
+		layer?.addSublayer(iconLayer)
+		
+		// label layer
+		_labelLayer.string = "Alan please change this."
+		_labelLayer.contentsScale = NSScreen.main?.backingScaleFactor ?? 1.0
+		_labelLayer.font = NSFont.systemFont(ofSize: 1.0, weight: .light)
+		_labelLayer.fontSize = 8.0
+		_labelLayer.foregroundColor = NSColor.fromHex("#3c3c3c").withAlphaComponent(0.75).cgColor
+		_labelLayer.frame.size = _labelLayer.preferredFrameSize()
+		_labelLayer.frame.origin = NSMakePoint(flagRect.maxX + CanvasGroup.LabelOffset, flagRect.midY - _labelLayer.frame.height * 0.5)
+		_labelLayer.frame.size.width = mainFrame.width - _labelLayer.frame.minX
+		_labelLayer.truncationMode = .middle
+		layer?.addSublayer(_labelLayer)
 		
 		ContextMenu.addItem(withTitle: "Submerge", action: #selector(CanvasGroup.onSubmerge), keyEquivalent: "")
 	}
