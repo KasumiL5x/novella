de581c1cefaa48dff55b097313f9083dfa31804c
Daniel Green <KasumiL5x@gmail.com>
2018-12-31 09:57:18 +0000

    Added a placeholder ConditionFunctionEditor storyboard.
    This is the first step in a change to the model: functions and conditions will become identifiable and reusable between elements. They will be assigned from a dropdown in the inspector and created/edited in the editor.

---
+++ novella/novella/ViewControllers/ConditionFunctionEditorViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ConditionFunctionEditorViewController.swift
+//  novella
+//
+//  Created by Daniel Green on 31/12/2018.
+//  Copyright © 2018 dgreen. All rights reserved.
+//
+
+import Cocoa
+
+class ConditionFunctionEditorViewController: NSViewController {
+}
