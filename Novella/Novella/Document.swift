019fe38cbc92c112a94efaf05f9353d883d49d9a
Daniel Green <KasumiL5x@gmail.com>
2019-03-22 11:50:09 +0000

    Added SAVING (but not loading) in the Document class.  It's almost identical to NVStoryJSON but I want to keep the save and export completely separate.

--- Novella/Novella/Document.swift
+++ Novella/Novella/Document.swift
@@ -34,14 +34,241 @@ class Document: NSDocument {
 	}
 
 	override func data(ofType typeName: String) throws -> Data {
-		let jsonString = "{}"
+		let jsonString = saveJSON()
+		if jsonString.isEmpty {
+			Swift.print("Tried to save JSON but the resulting string was empty.")
+			throw NSError(domain: NSOSStatusErrorDomain, code: writErr, userInfo: nil)
+		}
+		
 		guard let jsonData = jsonString.data(using: .utf8) else {
-			throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+			Swift.print("Tried to save JSON but failed to get Data from the JSON string.")
+			throw NSError(domain: NSOSStatusErrorDomain, code: writErr, userInfo: nil)
 		}
 		return jsonData
 	}
 
 	override func read(from data: Data, ofType typeName: String) throws {
+	}
+}
+
+extension Document {
+	private func saveJSON() -> String {
+		// Note: This function is almost identical to NVStory::toJSON(), but it includes extra editor-only information.
+		//       It was decided to keep them entirely separate as they shouldn't match or rely on each other.
+		var root: JSONDict = [:]
+		
+		// variables
+		var variables: [JSONDict] = []
+		Story.Variables.forEach{ (variable) in
+			var entry: JSONDict = [:]
+			entry["id"] = variable.UUID.uuidString
+			entry["label"] = variable.Name
+			entry["constant"] = variable.Constant
+			switch variable.InitialValue.Raw.type {
+			case .boolean:
+				entry["value"] = variable.InitialValue.Raw.asBool
+			case .integer:
+				entry["value"] = variable.InitialValue.Raw.asInt
+			case .double:
+				entry["value"] = variable.InitialValue.Raw.asDouble
+			}
+			variables.append(entry)
+		}
+		root["variables"] = variables
+		
+		// functions
+		var functions: [JSONDict] = []
+		Story.Functions.forEach{ (function) in
+			var entry: JSONDict = [:]
+			entry["id"] = function.UUID.uuidString
+			entry["code"] = function.Code
+			functions.append(entry)
+		}
+		root["functions"] = functions
+		
+		// conditions
+		var conditions: [JSONDict] = []
+		Story.Conditions.forEach{ (condition) in
+			var entry: JSONDict = [:]
+			entry["id"] = condition.UUID.uuidString
+			entry["code"] = condition.Code
+			conditions.append(entry)
+		}
+		root["conditions"] = conditions
+		
+		// selectors
+		var selectors: [JSONDict] = []
+		Story.Selectors.forEach{ (selector) in
+			var entry: JSONDict = [:]
+			entry["id"] = selector.UUID.uuidString
+			entry["code"] = selector.Code
+			selectors.append(entry)
+		}
+		root["selectors"] = selectors
+		
+		// entities (and tags)
+		var entities: [JSONDict] = []
+		Story.Entities.forEach{ (entity) in
+			var entry: JSONDict = [:]
+			entry["id"] = entity.UUID.uuidString
+			entry["label"] = entity.Label
+			entry["desc"] = entity.Description
+			entry["tags"] = entity.Tags
+			entities.append(entry)
+		}
+		root["entities"] = entities
+		
+		// event links
+		var eventLinks: [JSONDict] = []
+		Story.EventLinks.forEach{ (eventLink) in
+			var entry: JSONDict = [:]
+			entry["id"] = eventLink.UUID.uuidString
+			entry["origin"] = eventLink.Origin.UUID.uuidString
+			entry["dest"] = eventLink.Destination?.UUID.uuidString ?? ""
+			entry["function"] = eventLink.Function?.UUID.uuidString ?? ""
+			entry["condition"] = eventLink.Condition?.UUID.uuidString ?? ""
+			eventLinks.append(entry)
+		}
+		root["eventLinks"] = eventLinks
+		
+		// sequence links
+		var sequenceLinks: [JSONDict] = []
+		Story.SequenceLinks.forEach{ (seqLink) in
+			var entry: JSONDict = [:]
+			entry["id"] = seqLink.UUID.uuidString
+			entry["origin"] = seqLink.Origin.UUID.uuidString
+			entry["dest"] = seqLink.Destination?.UUID.uuidString ?? ""
+			entry["function"] = seqLink.Function?.UUID.uuidString ?? ""
+			entry["condition"] = seqLink.Condition?.UUID.uuidString ?? ""
+			sequenceLinks.append(entry)
+		}
+		root["sequenceLinks"] = sequenceLinks
+		
+		// groups
+		var groups: [JSONDict] = []
+		Story.Groups.forEach{ (group) in
+			groups.append(groupToJSON(group: group))
+		}
+		root["groups"] = groups
+		
+		// sequences
+		var sequences: [JSONDict] = []
+		Story.Sequences.forEach{ (sequence) in
+			sequences.append(sequenceToJSON(sequence: sequence))
+		}
+		root["sequences"] = sequences
+		
+		// discoverable (as a derivative of sequence)
+		var discoverables: [JSONDict] = []
+		Story.Discoverables.forEach{ (discoverable) in
+			var entry = sequenceToJSON(sequence: discoverable)
+			entry["tangibility"] = discoverable.Tangibility.toString
+			entry["functionality"] = discoverable.Functionality.toString
+			entry["clarity"] = discoverable.Clarity.toString
+			entry["delivery"] = discoverable.Delivery.toString
+			discoverables.append(entry)
+		}
+		root["discoverables"] = discoverables
+		
+		// events
+		var events: [JSONDict] = []
+		Story.Events.forEach{ (event) in
+			var entry: JSONDict = [:]
+			entry["id"] = event.UUID.uuidString
+			entry["label"] = event.Label
+			entry["parallel"] = event.Parallel
+			entry["topmost"] = event.Topmost
+			entry["maxactivations"] = event.MaxActivations
+			entry["keepalive"] = event.KeepAlive
+			entry["condition"] = event.PreCondition?.UUID.uuidString ?? ""
+			entry["entryfunction"] = event.EntryFunction?.UUID.uuidString ?? ""
+			entry["dofunction"] = event.DoFunction?.UUID.uuidString ?? ""
+			entry["exitfunction"] = event.ExitFunction?.UUID.uuidString ?? ""
+			entry["instigators"] = event.Instigators?.UUID.uuidString ?? ""
+			entry["targets"] = event.Targets?.UUID.uuidString ?? ""
+			
+			let pos = Positions[event.UUID] ?? CGPoint.zero
+			entry["position"] = [
+				"x": pos.x,
+				"y": pos.y
+			]
+			
+			Swift.print("TODO: Export attributes for Events.")
+			events.append(entry)
+		}
+		root["events"] = events
+		
+		// main group
+		root["maingroup"] = groupToJSON(group: Story.MainGroup)
+		
+		// valid JSON object check
+		if !JSONSerialization.isValidJSONObject(root) {
+			Swift.print("JSON object is invalid.")
+			return ""
+		}
+		
+		// get data
+		guard let jsonData = try? JSONSerialization.data(withJSONObject: root, options: [.prettyPrinted]) else {
+			Swift.print("Failed to get JSON Data.")
+			return ""
+		}
+		
+		guard let jsonString = String(data: jsonData, encoding: .utf8) else {
+			Swift.print("Failed to get JSON String from Data.")
+			return ""
+		}
+		
+		return jsonString
+	}
+	
+	private func groupToJSON(group: NVGroup) -> JSONDict {
+		var entry: JSONDict = [:]
+		entry["id"] = group.UUID.uuidString
+		entry["label"] = group.Label
+		entry["topmost"] = group.Topmost
+		entry["maxactivations"] = group.MaxActivations
+		entry["keepalive"] = group.KeepAlive
+		entry["condition"] = group.PreCondition?.UUID.uuidString ?? ""
+		entry["entryfunction"] = group.EntryFunction?.UUID.uuidString ?? ""
+		entry["exitFunction"] = group.ExitFunction?.UUID.uuidString ?? ""
+		entry["entry"] = group.Entry?.UUID.uuidString ?? ""
+		entry["sequences"] = group.Sequences.map{$0.UUID.uuidString}
+		entry["links"] = group.SequenceLinks.map{$0.UUID.uuidString}
+		entry["groups"] = group.Groups.map{$0.UUID.uuidString}
+		
+		let pos = Positions[group.UUID] ?? CGPoint.zero
+		entry["position"] = [
+			"x": pos.x,
+			"y": pos.y
+		]
+		
+		Swift.print("TODO: Export attributes for Groups.")
+		return entry
+	}
+	
+	private func sequenceToJSON(sequence: NVSequence) -> JSONDict {
+		var entry: JSONDict = [:]
+		entry["id"] = sequence.UUID.uuidString
+		entry["label"] = sequence.Label
+		entry["parallel"] = sequence.Parallel
+		entry["topmost"] = sequence.Topmost
+		entry["maxactivations"] = sequence.MaxActivations
+		entry["keepalive"] = sequence.KeepAlive
+		entry["condition"] = sequence.PreCondition?.UUID.uuidString ?? ""
+		entry["entryfunction"] = sequence.EntryFunction?.UUID.uuidString ?? ""
+		entry["exitfunction"] = sequence.ExitFunction?.UUID.uuidString ?? ""
+		entry["entry"] = sequence.Entry?.UUID.uuidString ?? ""
+		entry["events"] = sequence.Events.map{$0.UUID.uuidString}
+		entry["links"] = sequence.EventLinks.map{$0.UUID.uuidString}
+		
+		let pos = Positions[sequence.UUID] ?? CGPoint.zero
+		entry["position"] = [
+			"x": pos.x,
+			"y": pos.y
+		]
+		
+		Swift.print("TODO: Export attributes for Sequences.")
+		return entry
 	}
 }
 
